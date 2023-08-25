6dcea167bdb19f82fabc7aece44d7d97640a0608
Yuvan <yuviyuvan15@gmail.com>
2023-08-25 22:02:54 +0530

    Create twoSumLinear.cpp

---
+++ twoSumLinear.cpp
@@ -0,0 +1,16 @@
+class Solution
+{
+public:
+    vector<int> twoSum(vector<int> nums, int target)
+    {
+        map<int,int>m;
+        for(int i=0;i<nums.size();i++){
+            if(m.count(target-nums[i])){  
+                return {m[target-nums[i]],i};
+            }
+            m[nums[i]]=i;
+        }
+        return {};
+
+    }
+};
