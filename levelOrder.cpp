e4ccb11bfdbb767d16d90731e77473ac7424b935
Yuvan <yuviyuvan15@gmail.com>
2023-10-26 21:09:44 +0530

    Create levelOrder.cpp

---
+++ levelOrder.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        if(!root)return {};
+        queue<TreeNode*>q;
+        q.push(root);
+        vector<vector<int>>ans;
+        while(!q.empty()){
+            int siz=q.size();
+            vector<int>temp;
+            while(siz--){
+                TreeNode* node=q.front();
+                temp.push_back(node->val);
+                q.pop();
+                if(node->left)q.push(node->left);
+                if(node->right)q.push(node->right);
+            }
+            ans.push_back(temp);
+        }
+        return ans;
+    }
+};
